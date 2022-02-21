af572cdaf92c665c6df6ec3e76d58c7b18180ae3
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-02-21 16:56:46 -0500

    :tada: start using actix

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Temps
+
+
+## Backend
+
+Install cargo-watch
+```bash
+cargo install cargo-watch
+```
+
+Run
+```bash
+cargo-watch -x 'run --bin backend'
+```
+
+
+## Frontend
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::vec::Vec;
 use std::env;
 use reqwest;
-use tokio;
 use serde::Serialize;
 use serde::Deserialize;
 
@@ -16,19 +15,38 @@ struct GeoCoding {
     state: String
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+// struct Location {
+//     longitude: f32,
+//     latitude: f32,
+// }
+
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_location)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+#[get("/locations/{name}")]
+async fn get_location(req: HttpRequest) -> impl Responder {
+    let name: String = req.match_info().get("name").unwrap().parse().unwrap();
+    HttpResponse::Ok().body(format!("Hello yo from {}!", name))
+}
+
+fn find_location(location: &str) ->  Result<(), Box<dyn std::error::Error>> {
     const API_KEY: &str = env!("OPEN_WEATHER_MAP_API_KEY", "$OPEN_WEATHER_MAP_API_KEY is not set");
 
     const API_URL: &str = "http://api.openweathermap.org/geo/1.0/direct?q=";
-    let city_name = "Guatemala";
-    let request = format!("{}{}&appid={}", API_URL, city_name, API_KEY);
+    let request = format!("{}{}&appid={}", API_URL, location, API_KEY);
     println!("REQUEST: {}", request);
 
-    let resp = reqwest::get(request)
-        .await?
-        .json::<Vec<GeoCoding>>()
-        .await?;
+    let resp = reqwest::blocking::get(request)?.json::<Vec<GeoCoding>>();
     println!("{:?}", resp);
     Ok(())
 }
