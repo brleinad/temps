bd599d0c27b47d71e2ba518100df3fa2b7451bc8
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-02-22 16:38:50 -0500

    :sparkles: add cors  and fix port for release

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,6 @@
-
 use actix_web::{App, HttpServer};
+use actix_web::http::header;
+use actix_cors::Cors;
 
 mod types;
 mod services;
@@ -9,11 +10,18 @@ use services::*;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("https://www.temps-frontend.render.com")
+            .allowed_methods(vec!["GET"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600);
         App::new()
+            .wrap(cors)
             .service(locations_service)
             .service(forecast_service)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8088")?
     .run()
     .await
 }
