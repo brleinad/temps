d4308f5416231c7a4e8c95614b1d5d3b4efe5a0d
Daniel Rodas Bautista <daniel.bautista@tivix.com>
2022-04-06 10:05:46 -0400

    :rocket: deploy to fly.io

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .service(locations_service)
             .service(forecast_service)
     })
-    .bind("0.0.0.0:8088")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
